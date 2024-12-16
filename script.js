f29dd6ddf501be1f12fa09c37e9f7491cb86dd16
Yuthish231 <140402549+Yuthish231@users.noreply.github.com>
2024-12-16 11:33:00 +0530

    script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cards=document.getElementsByClassName("cards")[0];
 
     const validate = (username) => {
-        const usernameRegex = /^[a-zA-Z][a-zA-Z0-9_]{5,20}$/;
+        const usernameRegex = /^[a-zA-Z][a-zA-Z0-9_]+$/;
         if (username === "") {
             alert("Please enter an Username!");
             return false;
